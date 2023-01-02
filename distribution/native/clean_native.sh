ad1e56bc459ccf0a81d8d625fa99023c9c39a568
tison <wander4096@gmail.com>
2023-01-02 10:21:22 +0800

    fix(native): no-fallback and use native-maven-plugin
    Signed-off-by: tison <wander4096@gmail.com>

--- distribution/native/clean_native.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-[ -f hawkeye-native ] && rm hawkeye-native || echo "hawkeye-native not found."
--- distribution/native/make_native.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-"$JAVA_HOME"/bin/native-image \
-    -cp ../../hawkeye-command/target/hawkeye-command.jar \
-    io.korandoru.hawkeye.command.HawkEyeCommandMain \
-    hawkeye-native
