df5422bcd66ffb3baf5ab2cf3a33109125ca64cd
tison <wander4096@gmail.com>
2023-01-03 14:52:11 +0800

    feat: Add HeaderSource
    Signed-off-by: tison <wander4096@gmail.com>

---
+++ hawkeye-core/src/main/java/io/korandoru/hawkeye/core/HeaderSource.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2023 Korandoru Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.korandoru.hawkeye.core;
+
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * A wrapper class of license template text
+ */
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract sealed class HeaderSource permits LiteralHeaderSource, UrlHeaderSource {
+
+    private final String content;
+    private final boolean inline;
+
+    public abstract boolean isFromUrl(URL location);
+
+    public static HeaderSource of(String inlineHeader, String headerPath, ResourceFinder finder) {
+        return of(inlineHeader, headerPath, finder, StandardCharsets.UTF_8);
+    }
+
+    public static HeaderSource of(String inlineHeader, String headerPath, ResourceFinder finder, Charset charset) {
+        if (StringUtils.isNotEmpty(inlineHeader)) {
+            return new LiteralHeaderSource(inlineHeader);
+        }
+
+        if (StringUtils.isNotEmpty(headerPath)) {
+            try {
+                final URL headerUrl = finder.findResource(headerPath);
+                return new UrlHeaderSource(headerUrl, charset);
+            } catch (Exception e) {
+                throw new IllegalArgumentException(
+                        "Cannot read header document " + headerPath + ". Cause: " + e.getMessage(), e);
+            }
+        }
+
+        throw new IllegalArgumentException("Either inlineHeader or header path needs to be specified");
+    }
+}
---
+++ hawkeye-core/src/main/java/io/korandoru/hawkeye/core/UrlHeaderSource.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2023 Korandoru Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.korandoru.hawkeye.core;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Objects;
+import org.apache.commons.io.IOUtils;
+
+public final class UrlHeaderSource extends HeaderSource {
+    private final URL url;
+
+    public UrlHeaderSource(URL url, Charset charset) throws IOException {
+        super(IOUtils.toString(url, charset), false);
+        this.url = url;
+    }
+
+    @Override
+    public boolean isFromUrl(URL location) {
+        return Objects.equals(this.url, location);
+    }
+
+    @Override
+    public String toString() {
+        return url + ": " + getContent();
+    }
+}
