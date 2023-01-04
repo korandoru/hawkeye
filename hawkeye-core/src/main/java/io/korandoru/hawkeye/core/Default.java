6de9a93f91a69a41053cd049600c7cd03a8ba686
tison <wander4096@gmail.com>
2023-01-04 20:48:38 +0800

    feat: Add helper classes
    Signed-off-by: tison <wander4096@gmail.com>

---
+++ hawkeye-core/src/main/java/io/korandoru/hawkeye/core/Default.java
@@ -0,0 +1,253 @@
+/*
+ * Copyright 2023 Korandoru Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.korandoru.hawkeye.core;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Default {
+  public static final String[] INCLUDE = new String[]{"**"};
+
+  public static final String[] EXCLUDES = {
+      // Miscellaneous typical temporary files
+      "**/*~",
+      "**/#*#",
+      "**/.#*",
+      "**/%*%",
+      "**/._*",
+      "**/.repository/**",
+      "**/*.lck",
+
+      // CVS
+      "**/CVS",
+      "**/CVS/**",
+      "**/.cvsignore",
+
+      // RCS
+      "**/RCS",
+      "**/RCS/**",
+
+      // SCCS
+      "**/SCCS",
+      "**/SCCS/**",
+
+      // Visual SourceSafe
+      "**/vssver.scc",
+
+      // Subversion
+      "**/.svn",
+      "**/.svn/**",
+
+      // Arch
+      "**/.arch-ids",
+      "**/.arch-ids/**",
+
+      // Bazaar
+      "**/.bzr",
+      "**/.bzr/**",
+
+      // SurroundSCM
+      "**/.MySCMServerInfo",
+
+      // Mac
+      "**/.DS_Store",
+
+      // Docker
+      ".dockerignore",
+
+      // Serena Dimensions Version 10
+      "**/.metadata",
+      "**/.metadata/**",
+
+      // Mercurial
+      "**/.hg",
+      "**/.hg/**",
+      "**/.hgignore",
+
+      // git
+      "**/.git",
+      "**/.git/**",
+      "**/.gitattributes",
+      "**/.gitignore",
+      "**/.gitkeep",
+      "**/.gitmodules",
+
+      // GitHub
+      "**/.github",
+      "**/.github/**",
+
+      // BitKeeper
+      "**/BitKeeper",
+      "**/BitKeeper/**",
+      "**/ChangeSet",
+      "**/ChangeSet/**",
+
+      // darcs
+      "**/_darcs",
+      "**/_darcs/**",
+      "**/.darcsrepo",
+      "**/.darcsrepo/**",
+      "**/-darcs-backup*",
+      "**/.darcs-temp-mail",
+
+      // maven project's temporary files
+      "**/target/**",
+      "**/test-output/**",
+      "**/release.properties",
+      "**/dependency-reduced-pom.xml",
+      "**/release-pom.xml",
+      "**/pom.xml.releaseBackup",
+      "**/pom.xml.versionsBackup",
+
+      // Node
+      "**/node/**",
+      "**/node_modules/**",
+
+      // code coverage tools
+      "**/cobertura.ser",
+      "**/.clover/**",
+      "**/jacoco.exec",
+
+      // eclipse project files
+      "**/.classpath",
+      "**/.project",
+      "**/.settings/**",
+
+      // IDEA projet files
+      "**/*.iml",
+      "**/*.ipr",
+      "**/*.iws",
+      "**/.idea/**",
+
+      // Netbeans
+      "**/nb-configuration.xml",
+
+      // Hibernate Validator Annotation Processor
+      "**/.factorypath",
+
+      // descriptors
+      "**/MANIFEST.MF",
+
+      // License files
+      "**/LICENSE",
+      "**/LICENSE_HEADER",
+
+      // binary files - images
+      "**/*.jpg",
+      "**/*.png",
+      "**/*.gif",
+      "**/*.ico",
+      "**/*.bmp",
+      "**/*.tiff",
+      "**/*.tif",
+      "**/*.cr2",
+      "**/*.xcf",
+
+      // binary files - programs
+      "**/*.class",
+      "**/*.exe",
+      "**/*.dll",
+      "**/*.so",
+
+      // checksum files
+      "**/*.md5",
+      "**/*.sha1",
+      "**/*.sha256",
+      "**/*.sha512",
+
+      // Security files
+      "**/*.asc",
+      "**/*.jks",
+      "**/*.keytab",
+      "**/*.lic",
+      "**/*.p12",
+      "**/*.pub",
+
+      // binary files - archives
+      "**/*.jar",
+      "**/*.zip",
+      "**/*.rar",
+      "**/*.tar",
+      "**/*.tar.gz",
+      "**/*.tar.bz2",
+      "**/*.gz",
+      "**/*.7z",
+
+      // ServiceLoader files
+      "**/META-INF/services/**",
+
+      // Markdown files
+      "**/*.md",
+
+      // Office documents
+      "**/*.xls",
+      "**/*.doc",
+      "**/*.odt",
+      "**/*.ods",
+      "**/*.pdf",
+
+      // Travis
+      "**/.travis.yml",
+
+      // AppVeyor
+      "**/.appveyor.yml",
+      "**/appveyor.yml",
+
+      // CircleCI
+      "**/.circleci",
+      "**/.circleci/**",
+
+      // SourceHut
+      "**/.build.yml",
+
+      // Maven 3.3+ configs
+      "**/jvm.config",
+      "**/maven.config",
+
+      // Wrappers
+      "**/gradlew",
+      "**/gradlew.bat",
+      "**/gradle-wrapper.properties",
+      "**/mvnw",
+      "**/mvnw.cmd",
+      "**/maven-wrapper.properties",
+      "**/MavenWrapperDownloader.java",
+
+      // flash
+      "**/*.swf",
+
+      // json files
+      "**/*.json",
+
+      // fonts
+      "**/*.svg",
+      "**/*.eot",
+      "**/*.otf",
+      "**/*.ttf",
+      "**/*.woff",
+      "**/*.woff2",
+
+      // logs
+      "**/*.log",
+
+      // office documents
+      "**/*.xlsx",
+      "**/*.docx",
+      "**/*.ppt",
+      "**/*.pptx",
+  };
+}
---
+++ hawkeye-core/src/main/java/io/korandoru/hawkeye/core/document/DocumentFactory.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2023 Korandoru Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.korandoru.hawkeye.core.document;
+
+import io.korandoru.hawkeye.core.header.HeaderDefinition;
+import java.io.File;
+import java.nio.charset.Charset;
+import java.util.Map;
+import org.apache.commons.io.FilenameUtils;
+
+@SuppressWarnings("unused")
+public final class DocumentFactory {
+
+    private final Map<String, String> mapping;
+    private final Map<String, HeaderDefinition> definitions;
+    private final File basedir;
+    private final Charset encoding;
+    private final String[] keywords;
+    private final DocumentPropertiesLoader documentPropertiesLoader;
+
+    public DocumentFactory(
+            final File basedir,
+            final Map<String, String> mapping,
+            final Map<String, HeaderDefinition> definitions,
+            final Charset encoding,
+            final String[] keywords,
+            final DocumentPropertiesLoader documentPropertiesLoader
+    ) {
+        this.mapping = mapping;
+        this.definitions = definitions;
+        this.basedir = basedir;
+        this.encoding = encoding;
+        this.keywords = keywords.clone();
+        this.documentPropertiesLoader = documentPropertiesLoader;
+    }
+
+    public Document createDocuments(final String file) {
+        return getWrapper(file);
+    }
+
+    private Document getWrapper(final String file) {
+        final String lowerFileName = FilenameUtils.getName(file).toLowerCase();
+
+        String headerType = mapping.get("");
+        for (Map.Entry<String, String> entry : mapping.entrySet()) {
+            String lowerKey = entry.getKey().toLowerCase();
+            if (lowerFileName.endsWith("." + lowerKey) || lowerFileName.equals(lowerKey)) {
+                headerType = entry.getValue().toLowerCase();
+                break;
+            }
+        }
+
+        return new Document(
+                new File(basedir, file),
+                definitions.get(headerType),
+                encoding,
+                keywords,
+                documentPropertiesLoader);
+    }
+
+}
