8fa8fccc5693e5a8bd080f46af22b7772b60b70f
tison <wander4096@gmail.com>
2023-01-05 13:47:11 +0800

    try implement LicenseRemover
    Signed-off-by: tison <wander4096@gmail.com>

--- hawkeye-core/src/main/java/io/korandoru/hawkeye/core/LicenseRemover.java
+++ hawkeye-core/src/main/java/io/korandoru/hawkeye/core/LicenseRemover.java
@@ -5,33 +5,36 @@ import io.korandoru.hawkeye.core.document.Document;
 import io.korandoru.hawkeye.core.header.Header;
 import io.korandoru.hawkeye.core.model.HawkEyeConfig;
 
-public class LicenseFormatter extends LicenseProcessor {
+public class LicenseRemover extends LicenseProcessor {
 
-    public LicenseFormatter(HawkEyeConfig config) {
-        super(config, Report.Action.FORMAT);
+    public LicenseRemover(HawkEyeConfig config) {
+        super(config, Report.Action.REMOVE);
     }
 
     @Override
     protected void onHeaderNotFound(Document document, Header header, Report report) {
-        if (document.headerDetected()) {
-            document.removeHeader();
-            report.add(document.getFile(), Report.Result.REPLACED);
-        } else {
-            report.add(document.getFile(), Report.Result.ADDED);
-        }
-        document.updateHeader(header);
-        document.save();
+        remove(document, report);
     }
 
     @Override
     protected void onExistingHeader(Document document, Header header, Report report) {
-        report.add(document.getFile(), Report.Result.NOOP);
+        remove(document, report);
+    }
+
+    private void remove(Document document, Report report) {
+        if (document.headerDetected()) {
+            document.removeHeader();
+            document.save();
+            report.add(document.getFile(), Report.Result.REMOVED);
+        } else {
+            report.add(document.getFile(), Report.Result.NOOP);
+        }
     }
 
     public static void main(String[] args) throws Exception {
         final var mapper = new TomlMapper();
         final var config = mapper.readValue(HawkEyeConfig.class.getResource("/hawkeye.toml"), HawkEyeConfig.class);
-        final var checker = new LicenseFormatter(config);
+        final var checker = new LicenseRemover(config);
         final var report = checker.call();
         report.getResults().forEach((path, result) -> {
             if (result != Report.Result.NOOP) {
