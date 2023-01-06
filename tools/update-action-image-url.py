52768a7bdb182bc7b30ab14aecf2ca81879a038a
tison <wander4096@gmail.com>
2023-01-06 16:25:56 +0800

    chore: tidy code
    Signed-off-by: tison <wander4096@gmail.com>

--- tools/update-action-image-url.py
+++ tools/update-action-image-url.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 # Copyright 2023 Korandoru Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 
 import fileinput
 import re
