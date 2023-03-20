fa454e4be0a1091038e5dca31a963b86ec5dfe5f
tison <wander4096@gmail.com>
2023-03-20 23:35:55 +0800

    fix: generate constant VERSION based on project.version
    Signed-off-by: tison <wander4096@gmail.com>

--- hawkeye-command/src/main/java-filtered/io/korandoru/hawkeye/command/CommandConstantsFiltered.java
+++ hawkeye-command/src/main/java-filtered/io/korandoru/hawkeye/command/CommandConstantsFiltered.java
@@ -19,8 +19,8 @@ package io.korandoru.hawkeye.command;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
-public class CommandConstants {
+public class CommandConstantsFiltered {
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "${project.version}";
 
 }
