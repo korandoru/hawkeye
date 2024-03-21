ab5e8966a02d6ac165a6f862cdb4e2cdf2996b2b
tison <wander4096@gmail.com>
2024-03-21 16:11:26 +0800

    add
    Signed-off-by: tison <wander4096@gmail.com>

--- hawkeye-core/src/main/java/io/korandoru/hawkeye/core/GitHelper.java
+++ hawkeye-core/src/main/java/io/korandoru/hawkeye/core/GitHelper.java
@@ -8,17 +8,21 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class GitHelper {
+    private static final boolean NATIVE_LIBRARY_LOADED;
+
     private final long repo;
 
     static {
+        boolean nativeLibraryLoaded = false;
         try {
             // try dynamic library - the search path can be configured via "-Djava.library.path"
             System.loadLibrary("hawkeyejni");
-            log.debug("Loaded the hawkeyejni shared library.");
+            log.info("Loaded the hawkeyejni shared library.");
+            nativeLibraryLoaded = true;
         } catch (UnsatisfiedLinkError e) {
-            // ignore - try from classpath
-            log.debug("Unable to load the hawkeyejni shared library.", e);
+            log.warn("Unable to load the hawkeyejni shared library.", e);
         }
+        NATIVE_LIBRARY_LOADED = nativeLibraryLoaded;
     }
 
     public static GitHelper create(Path baseDir, GitModel config) {
@@ -26,13 +30,22 @@ public class GitHelper {
         if (checkIgnore.isDisable()) {
             return null;
         }
+
+        if (!NATIVE_LIBRARY_LOADED) {
+            if (checkIgnore.isAuto()) {
+                log.info("git.checkIgnore=auto is resolved to disable; unable to load the hawkeyejni shared library.");
+                return null;
+            }
+            throw new ResultException(ResultException.Code.GitError, "Unable to load the hawkeyejni shared library.");
+        }
+
         try {
             final long repo = discoverRepo(baseDir.toAbsolutePath().toString());
             if (checkIgnore.isAuto()) {
                 log.info("git.checkIgnore=auto is resolved to enable");
             }
             return new GitHelper(repo);
-        } catch (UnsatisfiedLinkError | ResultException e) {
+        } catch (ResultException e) {
             if (checkIgnore.isAuto()) {
                 log.info("git.checkIgnore=auto is resolved to disable", e);
                 return null;
