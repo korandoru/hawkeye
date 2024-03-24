21cc28567aa42a0747a800faefe51201b0d0f34d
tison <wander4096@gmail.com>
2024-03-24 08:57:20 +0800

    build: release automation
    Signed-off-by: tison <wander4096@gmail.com>

---
+++ release.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+# Copyright 2024 tison <wander4096@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, BooleanOptionalAction
+import fileinput
+from pathlib import Path
+import re
+import subprocess
+import tomllib
+
+parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+parser.add_argument('version', help='version or level to bump')
+parser.add_argument('--execute', '-x', action=BooleanOptionalAction, help='whether to execute the command (default to dry-run)')
+args = parser.parse_args()
+
+basedir = Path(__file__).parent.absolute()
+if args.execute:
+    cmd = ["cargo", "release", "version", args.version, "-x"]
+else:
+    cmd = ["cargo", "release", "version", args.version]
+
+# 1. Bump version
+subprocess.run(cmd, cwd=basedir, check=True)
+info = tomllib.loads((basedir / 'Cargo.toml').read_text())
+version = info['workspace']['package']['version']
+
+# 2. Update action.yml
+pattern = re.compile(r'docker://ghcr.io/korandoru/hawkeye-native.*')
+with fileinput.FileInput(basedir / 'action.yml', inplace=args.execute, backup='.bak') as content:
+    for line in content:
+        print(pattern.sub(f'docker://ghcr.io/korandoru/hawkeye-native:{version}', line), end='')
+
+if args.execute:
+    subprocess.run(["git", "add", "-A", "."], cwd=basedir, check=True)
+    subprocess.run(["git", "status"], cwd=basedir, check=True)
+    subprocess.run(["git", "commit", "-s", "-m", f"chore: release {version}"], cwd=basedir, check=True)
+
+# 3. Release
+if args.execute:
+    cmd = ["cargo", "release", "-x"]
+else:
+    cmd = ["cargo", "release"]
+subprocess.run(cmd, cwd=basedir, check=True)
